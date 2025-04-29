e36dea9d76f575e1692823d739ca47458231455a
Carlos Daniel <carlosdanieldev@gmail.com>
2025-04-28 22:25:29 -0300

    test: add unit tests for `HelpCommandHandler` to verify command handling and response

---
+++ src/tests/application/commands/handlers/HelpCommandHandler.test.ts
@@ -0,0 +1,17 @@
+import { HelpCommandHandler } from "@application/commands/handlers/HelpCommandHandler";
+import { Command } from "@domain/value-objects/Command";
+
+describe('HelpCommandHandler', () => {
+    it('should return the help message', () => {
+        const command = new Command('help');
+        const handler = new HelpCommandHandler();
+        const result = handler.canHandle(command);
+        expect(result).toBe(true);
+    })
+
+    it('should return the help message', () => {
+        const handler = new HelpCommandHandler();
+        const result = handler.handle();
+        expect(result).toBeDefined();
+    })
+})
