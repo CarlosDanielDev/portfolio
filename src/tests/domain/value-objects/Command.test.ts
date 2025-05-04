4aff8c0b7a2793313749c39cacef0070b3e6a8b4
Carlos Daniel <carlosdanieldev@gmail.com>
2025-05-04 15:18:57 -0300

    refactor: describe better the `Command`

--- src/tests/domain/value-objects/Command.test.ts
+++ src/tests/domain/value-objects/Command.test.ts
@@ -1,30 +1,32 @@
-import { Command } from '@domain/value-objects/Command';
+import { Command } from "@domain/value-objects/Command";
 
-describe('Command', () => {
-    it('should create a command with the given text', () => {
-        const command = new Command('ls');
-        expect(command.getText()).toBe('ls');
+describe("Command", () => {
+  describe("Anonymous user", () => {
+    it("should create a command with the given text", () => {
+      const command = new Command("ls");
+      expect(command.getText()).toBe("ls");
     });
 
-    it('should check if the command is a specific command', () => {
-        const command = new Command('ls');
-        expect(command.isCommand('ls')).toBe(true);
-        expect(command.isCommand('cd')).toBe(false);
+    it("should check if the command is a specific command", () => {
+      const command = new Command("ls");
+      expect(command.isCommand("ls")).toBe(true);
+      expect(command.isCommand("cd")).toBe(false);
     });
 
-    it('should get the argument of the command', () => {
-        const command = new Command('ls -l');
-        expect(command.getArgument('ls ')).toBe('-l');
-    });
-   
-    it('should check if the command starts with a specific prefix', () => {
-        const command = new Command('ls -l');
-        expect(command.startsWithCommand('ls')).toBe(true);
-        expect(command.startsWithCommand('cd')).toBe(false);
+    it("should get the argument of the command", () => {
+      const command = new Command("ls -l");
+      expect(command.getArgument("ls ")).toBe("-l");
     });
 
-
-    it('should throw an error if the command text is empty', () => {
-        expect(() => new Command('')).toThrow('Command text cannot be empty');
+    it("should check if the command starts with a specific prefix", () => {
+      const command = new Command("ls -l");
+      expect(command.startsWithCommand("ls")).toBe(true);
+      expect(command.startsWithCommand("cd")).toBe(false);
     });
+
+    it("should throw an error if the command text is empty", () => {
+      expect(() => new Command("")).toThrow("Command text cannot be empty");
+    });
+  });
 });
+
