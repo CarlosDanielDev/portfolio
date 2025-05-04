6ac9e0b35ff3c0651c59d9621f7272db994156f3
Carlos Daniel <carlosdanieldev@gmail.com>
2025-05-04 16:48:36 -0300

    refactor: cover more `stmts` in the `SkillsCommandHandler`

--- src/tests/application/commands/handlers/SkillsCommandHandler.test.ts
+++ src/tests/application/commands/handlers/SkillsCommandHandler.test.ts
@@ -1,24 +1,58 @@
 import { Command } from "@domain/value-objects/Command";
 import { SkillsCommandHandler } from "@application/commands/handlers/SkillsCommandHandler";
+import { ListCompanySkillsOutput } from "@/application/use-cases/ListCompanySkillsUseCase";
 
-describe('SkillsCommandHandler', () => {
-    it('should return the skills message', () => {
-        const handler = new SkillsCommandHandler(null);
-        const result = handler.handle();
-        expect(result).toBeDefined();
-    })
+describe("SkillsCommandHandler", () => {
+  describe("Anonymous user", () => {
+    it("should list the company valid skills", () => {
+      const selectedCompany: ListCompanySkillsOutput = {
+        companyId: "1",
+        companyName: "Test",
+        dateRange: "date-range",
+        position: "position",
+        skills: [
+          {
+            dateRange: "date-range",
+            description: "description",
+            id: "2",
+            level: "5",
+            name: "technology",
+          },
+        ],
+      };
+      const handler = new SkillsCommandHandler(selectedCompany);
+      const result = handler.handle();
 
-    it('should not handle other commands', () => {
-        const command = new Command('ls');
-        const handler = new SkillsCommandHandler(null);
-        const result = handler.canHandle(command);
-        expect(result).toBe(false);
-    })
+      expect(result).toBeDefined();
+      expect(result).not.toBe("No skills found for this company.");
 
-    it('should return the skills message', () => {
-        const command = new Command('skills');
-        const handler = new SkillsCommandHandler(null);
-        const result = handler.canHandle(command);
-        expect(result).toBe(true);
-    })
-})
+      const lsCommand = new Command("ls");
+      const cannotHandle = handler.canHandle(lsCommand);
+      expect(cannotHandle).toBe(false);
+
+      const skillsCommand = new Command("skills");
+      const canHandle = handler.canHandle(skillsCommand);
+      expect(canHandle).toBe(true);
+    });
+
+    it("should handle with a empty company", () => {
+      const selectedCompany: ListCompanySkillsOutput | null = null;
+      const handler = new SkillsCommandHandler(selectedCompany);
+      const result = handler.handle();
+      expect(result).toBe(
+        'No company selected. Use "cd <company_name>" to select a company.',
+      );
+
+      expect(result).toBeDefined();
+
+      const lsCommand = new Command("ls");
+      const cannotHandle = handler.canHandle(lsCommand);
+      expect(cannotHandle).toBe(false);
+
+      const skillsCommand = new Command("skills");
+      const canHandle = handler.canHandle(skillsCommand);
+      expect(canHandle).toBe(true);
+    });
+  });
+});
+
